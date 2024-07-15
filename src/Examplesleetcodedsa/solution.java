793033be48e6f47d974e3724b21e40cb1e1dee6c
bhavatharany03 <bhavatharanyeks@gmail.com>
2024-07-15 15:03:50 +0530

    rotate array

---
+++ src/Examplesleetcodedsa/solution.java
@@ -0,0 +1,23 @@
+package Examplesleetcodedsa;
+
+public class solution {
+        public void rotate(int[] nums, int k) {
+            k=k%nums.length;
+            reverse(nums,0,nums.length-1);
+            reverse(nums,0,k-1);
+            reverse(nums,k,nums.length-1);
+            for(int i:nums){
+                System.out.print(i);
+            }
+        }
+        public static void reverse(int[] nums,int start,int end){
+            while(start<end){
+                int temp=nums[start];
+                nums[start]=nums[end];
+                nums[end]=temp;
+                start++;
+                end--;
+            }
+        }
+    }
+
